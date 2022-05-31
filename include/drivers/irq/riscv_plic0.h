654608117c28ab962c0e8e2513e04d1cbda4de5c
Nils Wistoff <nwistoff@iis.ee.ethz.ch>
2022-05-31 23:08:41 +0200

    ariane: Re-enable PLIC
    Signed-off-by: Nils Wistoff <nwistoff@iis.ee.ethz.ch>

--- include/drivers/irq/riscv_plic0.h
+++ include/drivers/irq/riscv_plic0.h
@@ -14,8 +14,8 @@
  * this driver is confirmed to be working on other platforms. */
 #if !defined(CONFIG_PLAT_HIFIVE) && \
     !defined(CONFIG_PLAT_POLARFIRE) && \
-    !defined(CONFIG_PLAT_QEMU_RISCV_VIRT)
-#error "Check if this platform suppots a PLIC."
+    !defined(CONFIG_PLAT_QEMU_RISCV_VIRT) && \
+    !defined(CONFIG_PLAT_ARIANE)
 #endif
 
 /* tell the kernel we have the set trigger feature */
